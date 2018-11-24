ebd6e5e759dd7a1ad1bab0f3ad083079bca436d3
Kai Hildebrandt <kai.hildebrandt@uniberg.com>
2018-11-24 11:31:35 +0100

    Initial Commit

---
+++ watson-bar-icon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  watson-bar-icon
+//
+//  Created by Kai Hildebrandt on 24.11.18.
+//  Copyright © 2018 Kai Hildebrandt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ watson-bar-icon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  watson-bar-icon
+//
+//  Created by Kai Hildebrandt on 24.11.18.
+//  Copyright © 2018 Kai Hildebrandt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
